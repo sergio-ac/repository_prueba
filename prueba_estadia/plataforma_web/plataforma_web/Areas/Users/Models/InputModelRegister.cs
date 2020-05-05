5f40fbda1bbfef8612d9e5750f3ed2661e473509
sergio-ac <57115186+sergio-ac@users.noreply.github.com>
2020-05-05 12:18:53 -0500

    estadia_prueba
    primera prueba

---
+++ prueba_estadia/plataforma_web/plataforma_web/Areas/Users/Models/InputModelRegister.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace plataforma_web.Areas.Users.Models
+{
+    public class InputModelRegister
+    {
+        [Required(ErrorMessage = "El campo nombre es obligatorio.")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "El campo apellido es obligatorio")]
+        public string LastName { get; set; }
+        [Required(ErrorMessage = "El campo nid es obligatorio")]
+        public string NID { get; set; }
+        [Required(ErrorMessage = "El campo telefono es obligatorio")]
+        [DataType(DataType.PhoneNumber)]
+        [RegularExpression(@"^\(?([0-9]{2})\)?[-, ]?([0-9]{2})[-, ]?([0-9]{5})$", ErrorMessage = "El formato telefono ingresado no es válido.")]
+        public string PhoneNumber { get; set; }
+        [Required(ErrorMessage ="El campo correo electronico es obligatorio")]
+        [EmailAddress(ErrorMessage ="El correo electronico no es una dirección de correo electronico válida")]
+        public string Email { get; set; }
+        [Display(Name="Contraseña")]
+        [Required(ErrorMessage ="El campo contraseña es obligatorio")]
+        [StringLength(100,ErrorMessage ="El numero de caracteres de {0} debe ser al menos {2}.",MinimumLength =6)]
+        public string Password { get;set; }
+
+
+    }
+}
---
+++ prueba_estadia/plataforma_web/plataforma_web/Areas/Users/Pages/Account/Register.cshtml.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using plataforma_web.Areas.Users.Models;
+
+namespace plataforma_web.Areas.Users.Pages.Account
+{
+    public class RegisterModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+        [BindProperty]
+        public InputModel Input { get; set; }
+        public class InputModel : InputModelRegister
+        {
+            public IFormFile AvatarImage { get; set; }
+        }
+    }
+}
