71f1a78cbde34973f092df61c540f97425e616a8
sergio-ac <57115186+sergio-ac@users.noreply.github.com>
2020-05-07 14:08:20 -0500

    Primera conexion con sql

--- prueba_estadia/plataforma_web/plataforma_web/Data/Migrations/20200507175017_Migracion1.Designer.cs
+++ prueba_estadia/plataforma_web/plataforma_web/Data/Migrations/20200507175017_Migracion1.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using plataforma_web.Data;
 
 namespace plataforma_web.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20200507175017_Migracion1")]
+    partial class Migracion1
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
